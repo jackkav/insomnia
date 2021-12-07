cbce454b9f7a3e9b2ba7f90f6e92eeaa72a9207e
jackkav <jackkav@gmail.com>
2021-12-07 01:05:22 +0100

    crypto and util fix

--- packages/insomnia-web/vite.config.ts
+++ packages/insomnia-web/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    mainFields:['browser'],
+    alias: {
+     "crypto": path.resolve(__dirname, '../insomnia-app/node_modules/crypto-browserify'),
+    }
+  }
 })
