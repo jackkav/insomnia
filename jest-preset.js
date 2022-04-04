bc198e7dc7656e20bacc40d6e5ccdf231cf6938c
Jack Kavanagh <jackkav@gmail.com>
2022-04-04 16:59:03 +0100

    example

--- jest-preset.js
+++ jest-preset.js
@@ -1,16 +1,10 @@
 /** @type { import('@jest/types').Config.InitialOptions } */
 module.exports = {
   collectCoverage: false,
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   resetMocks: true,
   resetModules: true,
   testEnvironment: 'node',
   testRegex: ['.+\\.test\\.tsx?$'],
-  transform: { '^.+\\.tsx?$': 'ts-jest' },
   verbose: false,
 };
