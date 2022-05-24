626a816679f97591063a3bac459d80357d4b828a
Jack Kavanagh <jackkav@gmail.com>
2022-05-24 21:02:13 +0200

    clean up <Editable

--- packages/insomnia/src/ui/components/base/editable.tsx
+++ packages/insomnia/src/ui/components/base/editable.tsx
@@ -1,7 +1,5 @@
-import { autoBindMethodsForReact } from 'class-autobind-decorator';
-import React, { PureComponent } from 'react';
+import React, { useRef, useState } from 'react';
 
-import { AUTOBIND_CFG } from '../../../common/constants';
 import { KeydownBinder } from '../keydown-binder';
 
 export const shouldSave = (oldValue, newValue, preventBlank = false) => {
@@ -31,126 +29,94 @@ interface Props {
   preventBlank?: boolean;
 }
 
-interface State {
-  editing: boolean;
-}
+export const Editable = ({
+  value,
+  fallbackValue,
+  blankValue,
+  singleClick,
+  onEditStart,
+  preventBlank,
+  className,
+  onSubmit,
+  renderReadView,
+  ...extra
+}: Props) => {
+  const [isEditing, setEdit] = useState(false);
+  const inputRef = useRef(null);
+  const initialValue = value || fallbackValue;
 
-@autoBindMethodsForReact(AUTOBIND_CFG)
-export class Editable extends PureComponent<Props, State> {
-  state: State = {
-    editing: false,
+  const _handleEditStart = () => {
+    setEdit(true);
+
+    setTimeout(() => {
+      inputRef?.current?.focus();
+      inputRef?.current?.select();
+    });
+
+    // Used for drag and drop in parent component
+    if (onEditStart) {
+      onEditStart();
+    }
   };
 
-  _input: HTMLInputElement | null = null;
-
-  _handleSetInputRef(n: HTMLInputElement) {
-    this._input = n;
-  }
-
-  _handleSingleClickEditStart() {
-    if (this.props.singleClick) {
-      this._handleEditStart();
-    }
-  }
-
-  _handleEditStart() {
-    this.setState({
-      editing: true,
-    });
-    setTimeout(() => {
-      this._input?.focus();
-      this._input?.select();
-    });
-
-    if (this.props.onEditStart) {
-      this.props.onEditStart();
-    }
-  }
-
-  _handleEditEnd() {
-    const originalValue = this.props.value;
-
-    const newValue = this._input?.value.trim();
-
-    if (shouldSave(originalValue, newValue, this.props.preventBlank)) {
+  const _handleEditEnd = () => {
+    if (shouldSave(value, inputRef?.current?.value.trim(), preventBlank)) {
       // Don't run onSubmit for values that haven't been changed
-      this.props.onSubmit(newValue);
+      onSubmit(inputRef?.current?.value.trim());
     }
 
     // This timeout prevents the UI from showing the old value after submit.
     // It should give the UI enough time to redraw the new value.
-    setTimeout(
-      async () =>
-        this.setState({
-          editing: false,
-        }),
-      100,
+    setTimeout(() => setEdit(false), 100);
+  };
+
+  if (isEditing) {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.keyCode === 13) {
+        // Pressed Enter
+        _handleEditEnd();
+        return;
+      }
+      if (event.keyCode === 27) {
+        // Pressed Escape
+        // Prevent bubbling to modals and other escape listeners.
+        event.stopPropagation();
+
+        if (inputRef.current) {
+          // Set the input to the original value
+          inputRef.current.value = value;
+
+          _handleEditEnd();
+        }
+      }
+    };
+    return (
+      // KeydownBinder must be used here to properly stop propagation
+      // from reaching other scoped KeydownBinders
+      <KeydownBinder onKeydown={onKeyDown} scoped>
+        <input
+          {...extra}
+          className={`editable< ${className || ''}`}
+          type="text"
+          ref={inputRef}
+          defaultValue={initialValue}
+          onBlur={_handleEditEnd}
+        />
+      </KeydownBinder>
     );
-  }
+  } else {
+    const readViewProps = {
+      className: `editable ${className} ${!initialValue && 'empty'}`,
+      title: singleClick ? 'Click to edit' : 'Double click to edit',
+      onClick: () => singleClick && _handleEditStart(),
+      onDoubleClick: _handleEditStart,
+      ...extra,
+    };
 
-  _handleEditKeyDown(event: KeyboardEvent) {
-    if (event.keyCode === 13) {
-      // Pressed Enter
-      this._handleEditEnd();
-    } else if (event.keyCode === 27) {
-      // Pressed Escape
-      // Prevent bubbling to modals and other escape listeners.
-      event.stopPropagation();
-
-      if (this._input) {
-        // Set the input to the original value
-        this._input.value = this.props.value;
-
-        this._handleEditEnd();
-      }
-    }
-  }
-
-  render() {
-    const {
-      value,
-      fallbackValue,
-      blankValue,
-      singleClick,
-      onEditStart,
-      preventBlank,
-      className,
-      onSubmit,
-      renderReadView,
-      ...extra
-    } = this.props;
-    const { editing } = this.state;
-    const initialValue = value || fallbackValue;
-
-    if (editing) {
-      return (
-        // KeydownBinder must be used here to properly stop propagation
-        // from reaching other scoped KeydownBinders
-        <KeydownBinder onKeydown={this._handleEditKeyDown} scoped>
-          <input
-            {...extra}
-            className={`editable ${className || ''}`}
-            type="text"
-            ref={this._handleSetInputRef}
-            defaultValue={initialValue}
-            onBlur={this._handleEditEnd}
-          />
-        </KeydownBinder>
-      );
+    if (renderReadView) {
+      return renderReadView(initialValue, readViewProps);
     } else {
-      const readViewProps = {
-        className: `editable ${className} ${!initialValue && 'empty'}`,
-        title: singleClick ? 'Click to edit' : 'Double click to edit',
-        onClick: this._handleSingleClickEditStart,
-        onDoubleClick: this._handleEditStart,
-        ...extra,
-      };
-
-      if (renderReadView) {
-        return renderReadView(initialValue, readViewProps);
-      } else {
-        return <span {...readViewProps}>{initialValue || blankValue}</span>;
-      }
+      return <span {...readViewProps}>{initialValue || blankValue}</span>;
     }
   }
-}
+};
