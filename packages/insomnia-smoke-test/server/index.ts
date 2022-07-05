c6c96b3d743c80f3ae5b3cec305e9910b9a00ade
jackkav <jackkav@gmail.com>
2022-07-05 12:18:03 +0200

    add multer

--- packages/insomnia-smoke-test/server/index.ts
+++ packages/insomnia-smoke-test/server/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import fs from 'fs';
+import multer from 'multer';
 
 import { basicAuthRouter } from './basic-auth';
 import githubApi from './github-api';
@@ -53,6 +55,42 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
+
+// app.get('/multipart-response', (_, res) => {
+//   var form = new FormData();
+//   form.append('part1', 'part 1 data');
+//   form.append('part2', 'part 2 data');
+//   res.setHeader('x-Content-Type', 'multipart/form-data; boundary='+form._boundary);
+//   res.setHeader('Content-Type', 'text/plain');
+//   form.pipe(res);
+// });
+
+app.get('/multipart-form', (_, res) => {
+  res.send(`<form action="http://localhost:4010/upload-multipart" method="post" enctype="multipart/form-data">
+<p><input type="text" name="text" value="some text">
+<p><input type="file" name="fileToUpload">
+<p><button type="submit">Submit</button>
+</form>`);
+});
+
+const upload = multer({ dest: './public/data/uploads/' });
+app.post('/upload-multipart', upload.single('fileToUpload'), (req, res) => {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any
+  console.log(req.file, req.body);
+
+  if (req.file?.fieldname !== 'fileToUpload') {
+    return res.status(500).send('must include file');
+  }
+  const isMimetypeReadable = !!['yaml', 'json', 'xml'].filter(x => req.file?.mimetype.includes(x)).length;
+  const fileContents = !isMimetypeReadable ? '' : fs.readFileSync(req.file?.path).toString();
+  return res.status(200).send(JSON.stringify(req.file, null, '\t') + `
+${fileContents}`);
+});
+
+// HTTP Redirect
+app.get('/from', (_, res) => res.redirect(301, '/to'));
+app.get('/to', (_, res) => res.send('Hello, World!'));
 
 startGRPCServer(grpcPort).then(() => {
   app.listen(port, () => {
